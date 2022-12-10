3f7c3b3fdd4067dd2bfa99d128e990c4b8945a15
Antoine Choppin <antoine@japonophile.com>
2022-12-10 20:46:08 +0900

    Minor updates

--- pychat/pychat_client.py
+++ pychat/pychat_client.py
@@ -1,19 +1,22 @@
-import select, socket, sys
-from pychat_util import Room, Hall, Player
+import select
+import socket
+import sys
 import pychat_util
 
 READ_BUFFER = 4096
 
 if len(sys.argv) < 2:
-    print("Usage: Python3 client.py [hostname]", file = sys.stderr)
+    print("Usage: Python3 client.py [hostname]", file=sys.stderr)
     sys.exit(1)
 else:
     server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_connection.connect((sys.argv[1], pychat_util.PORT))
 
+
 def prompt():
-    print('>', end=' ', flush = True)
+    print('>', end=' ', flush=True)
+
 
 print("Connected to server\n")
 msg_prefix = ''
@@ -21,9 +24,10 @@ msg_prefix = ''
 socket_list = [sys.stdin, server_connection]
 
 while True:
-    read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+    read_sockets, write_sockets, error_sockets = \
+        select.select(socket_list, [], [])
     for s in read_sockets:
-        if s is server_connection: # incoming message 
+        if s is server_connection:  # incoming message
             msg = s.recv(READ_BUFFER)
             if not msg:
                 print("Server down!")
@@ -35,7 +39,7 @@ while True:
                 else:
                     sys.stdout.write(msg.decode())
                     if 'Please tell us your name' in msg.decode():
-                        msg_prefix = 'name: ' # identifier for name
+                        msg_prefix = 'name: '  # identifier for name
                     else:
                         msg_prefix = ''
                     prompt()
